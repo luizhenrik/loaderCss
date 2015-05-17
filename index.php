b1d5bb8d121f7e36f637af60f4eb09065539f2f7
luizhenrik <luhemeso@gmail.com>
2015-05-17 10:38:47 -0300

    primary commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="loader.css">
+</head>
+<body>
+<!-- fixed é opcional o unico que você precisa  é a div#loader -->
+	<div class="fixed"> 
+		<div id="loader">
+			<div id="loader1"></div>
+			<div id="loader2"></div>
+			<span id="text">Carregando</span>
+		</div>
+	</div>
+
+</body>
+</html>
